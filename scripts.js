ac62ddc47dc3a9d325d13ba707bb7b08e42d42fd
adnorm <acnorman@email.wm.edu>
2022-10-07 17:16:44 -0400

    v1.51 fixed undefined word bug

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 var adjective = ["Crooked", "Deep", "Even", "Flat", "Hilly", "Jagged", "Round", "Shallow", "Winding", "Fresh", "Verdant", "Rolling", "Babbling", "Jumping", "Old", "Willow", "Shady", "Calm", "Lucky", "Sunny", "Green", "Sleeping","Windy","Victory","Freedom","Deep", "Whisper","Liberty","Early","Morning","Spring","Blooming", "Dancing","Hidden","Golden","Rich","Quiet","Wandering","Honest","Truthful","Sliding","Singing","Fruitful","Lost","Roaming","Wild","Bucking","Sun","Moon","Flying","Goose", "Sweet","Muddy","Joyful","Sylvan","Friendly","Crimson","Blue","Big","Eager","Talking","Secret"]
 
 
-var object = ["Acres", "Pastures", "Grove", "Crest", "Fields", "Pines", "Stone", "Hollow", "Hickory","Roost", "Barn", "Willow", "Run", "Harvest","Trails", "Vista","Trace", "Cottage","Passage","Dream","Boots","Work","Plains","Glen","Haven","Vines","Skies","Woods",,"Oaks","Cedars","Bend","Range","Point","Sanctuary","Land","Hill","Bluff","Bridge","Dogs","Ramble","Bramble","Way","Heights","View","Pass"]
+var object = ["Acres", "Pastures", "Grove", "Crest", "Fields", "Pines", "Stone", "Hollow", "Hickory","Roost", "Barn", "Willow", "Run", "Harvest","Trails", "Vista","Trace", "Cottage","Passage","Dream","Boots","Work","Plains","Glen","Haven","Vines","Skies","Woods","Oaks","Cedars","Bend","Range","Point","Sanctuary","Land","Hill","Bluff","Bridge","Dogs","Ramble","Bramble","Way","Heights","View","Pass"]
 var list;
 
 function generator() {
